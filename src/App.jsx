3f6aab6c617f2123384fbb00c391f8551b2b9287
henrisagena <henrisianturisagena@gmail.com>
2024-06-06 09:07:50 -0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     {
       id: 1,
       title: 'Finish Progate React Course',
-      completed: false,
+      completed: false,  // Menganti nilai awal ke true
     },
     {
       id: 2,
@@ -19,15 +19,28 @@ function App() {
       completed: false,
     },
   ])
+  // Definisikan toggleCompleted di sini
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
 
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      {/* Teruskan function toggleCompleted ke component Todos */}
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
 }
+
+
 
 const styles = {
   container: {
